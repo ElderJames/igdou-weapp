98fd747642c9f953c384de8d147faf8c0be00960
ElderJames <shunjiey@hotmail.com>
2018-02-03 13:58:28 +0800

    fix(tabview): 修复定位变量的更新问题

--- src/components/tabView/tabview.ts
+++ src/components/tabView/tabview.ts
@@ -1,130 +1,129 @@
-import Component from 'component'
-import wepy from 'wepy';
+import Component from "component";
+import wepy from "wepy";
 
 export default class tabView extends Component {
+  windowWidth: number;
+  tabsCount: number;
+  tapStartTime: number;
+  startX: number;
+  tapStartX: number;
+  tapStartY: number;
 
-    windowWidth: number;
-    tabsCount: number;
-    tapStartTime: number;
-    startX: number;
-    tapStartX: number;
-    tapStartY: number;
+  data = {
+    tabs: ["收入", "支出", "通知", "我的"],
+    stv: {
+      windowWidth: 0,
+      lineWidth: 0,
+      offset: 0,
+      tStart: false
+    },
+    activeTab: 0
+  };
 
+  complated = {};
 
-    data = {
-        tabs: ['收入', '支出', '通知', '我的'],
-        stv: {
-            windowWidth: 0,
-            lineWidth: 0,
-            offset: 0,
-            tStart: false
-        },
-        activeTab: 0
+  watcher = {
+    activeTab(newValue, oldValue) {
+      console.log(`activeTab value: ${oldValue} -> ${newValue}`);
     }
+  };
 
-    complated = {
-        showActiveClass(index: number) {
-            console.log(index);
-            return this.data.activeTab == index ? "tab-active" : "";
-        }
+  async onLoad(options: any) {
+    try {
+      let { tabs } = this.data;
+      var res = await wepy.getSystemInfoSync();
+      console.log(res);
+      this.stv.lineWidth = res.windowWidth / this.data.tabs.length;
+      this.stv.windowWidth = res.windowWidth;
+      this.tabsCount = tabs.length;
+
+      this.$apply();
+    } catch (e) {
+      console.log("tabview error", e);
     }
+  }
 
-    async onLoad(options: any) {
-        try {
-            let { tabs } = this.data;
-            var res = await wepy.getSystemInfoSync()
-            console.log(res);
-            this.data.stv.lineWidth = res.windowWidth / this.data.tabs.length;
-            this.data.stv.windowWidth = res.windowWidth;
-            // this.stv: this.data.stv })
-            this.$apply();
-            this.tabsCount = tabs.length;
-        } catch (e) {
-            console.log('tabview error')
-        }
-    }
+  methods = {
+    handlerStart(e) {
+      let { clientX, clientY } = e.touches[0];
+      this.startX = clientX;
+      this.tapStartX = clientX;
+      this.tapStartY = clientY;
+      this.data.stv.tStart = true;
+      this.tapStartTime = e.timeStamp;
+      this.$apply();
+    },
 
-    methods = {
+    handlerMove(e) {
+      let { clientX, clientY } = e.touches[0];
+      let { stv } = this.data;
+      let offsetX = this.startX - clientX;
+      this.startX = clientX;
+      stv.offset += offsetX;
+      if (stv.offset <= 0) {
+        stv.offset = 0;
+      } else if (stv.offset >= stv.windowWidth * (this.tabsCount - 1)) {
+        stv.offset = stv.windowWidth * (this.tabsCount - 1);
+      }
+      this.stv = stv;
+      this.$apply();
+    },
 
-        handlerStart(e) {
-            let { clientX, clientY } = e.touches[0];
-            this.startX = clientX;
-            this.tapStartX = clientX;
-            this.tapStartY = clientY;
-            this.data.stv.tStart = true;
-            this.tapStartTime = e.timeStamp;
-            this.$apply();
-        },
+    handlerCancel(e) {},
 
-        handlerMove(e) {
-            let { clientX, clientY } = e.touches[0];
-            let { stv } = this.data;
-            let offsetX = this.startX - clientX;
-            this.startX = clientX;
-            stv.offset += offsetX;
-            if (stv.offset <= 0) {
-                stv.offset = 0;
-            } else if (stv.offset >= stv.windowWidth * (this.tabsCount - 1)) {
-                stv.offset = stv.windowWidth * (this.tabsCount - 1);
+    handlerEnd(e) {
+      let { clientX, clientY } = e.changedTouches[0];
+      let endTime = e.timeStamp;
+      let { tabs, stv, activeTab } = this.data;
+      let { offset, windowWidth } = stv;
+      //快速滑动
+      if (endTime - this.tapStartTime <= 300) {
+        //向左
+        if (Math.abs(this.tapStartY - clientY) < 50) {
+          if (this.tapStartX - clientX > 5) {
+            if (this.activeTab < this.tabsCount - 1) {
+              ++this.activeTab;
             }
-            this.data.stv = stv;
-            this.$apply();
-        },
-
-        handlerCancel(e) {
-
-        },
-
-        handlerEnd(e) {
-            let { clientX, clientY } = e.changedTouches[0];
-            let endTime = e.timeStamp;
-            let { tabs, stv, activeTab } = this.data;
-            let { offset, windowWidth } = stv;
-            //快速滑动
-            if (endTime - this.tapStartTime <= 300) {
-                //向左
-                if (Math.abs(this.tapStartY - clientY) < 50) {
-                    if (this.tapStartX - clientX > 5) {
-                        if (activeTab < this.tabsCount - 1) {
-                            ++activeTab;
-                        }
-                    } else {
-                        if (activeTab > 0) {
-                            --activeTab;
-                        }
-                    }
-                    stv.offset = stv.windowWidth * activeTab;
-                } else {
-                    //快速滑动 但是Y距离大于50 所以用户是左右滚动
-                    let page = Math.round(offset / windowWidth);
-                    if (activeTab != page) {
-                        activeTab = page;
-                    }
-                    stv.offset = stv.windowWidth * page;
-                }
-            } else {
-                let page = Math.round(offset / windowWidth);
-                if (activeTab != page) {
-                    activeTab = page;
-                }
-                stv.offset = stv.windowWidth * page;
+          } else {
+            if (this.activeTab > 0) {
+              --this.activeTab;
             }
-
-            stv.tStart = false;
-            this.data.stv = stv;
-            this.data.activeTab = activeTab;
-            this.$apply();
-        },
-
-        handlerTabTap(e) {
-            this._updateSelectedPage(e.currentTarget.dataset.index);
-            this.$apply();
+          }
+          stv.offset = stv.windowWidth * this.activeTab;
+        } else {
+          //快速滑动 但是Y距离大于50 所以用户是左右滚动
+          let page = Math.round(offset / windowWidth);
+          if (this.activeTab != page) {
+            this.activeTab = page;
+          }
+          stv.offset = stv.windowWidth * page;
         }
-    }
-    _updateSelectedPage(page) {
-        let { tabs, stv, activeTab } = this.data;
-        this.data.activeTab = page;
+      } else {
+        let page = Math.round(offset / windowWidth);
+        if (this.activeTab != page) {
+          this.activeTab = page;
+        }
         stv.offset = stv.windowWidth * page;
-        this.data.stv = stv;
+      }
+
+      stv.tStart = false;
+      this.stv = stv;
+      //   this.activeTab = this.activeTab;
+      this.$apply();
+
+      console.log(this.data);
+    },
+
+    handlerTabTap(e) {
+      this._updateSelectedPage(e.currentTarget.dataset.index);
+      this.$apply();
     }
+  };
+  _updateSelectedPage(page) {
+    let { tabs, stv, activeTab } = this.data;
+    stv.offset = stv.windowWidth * page;
+    this.stv = stv;
+    this.activeTab = page;
+    console.log(this.data);
+  }
 }
--- src/pages/study/index.ts
+++ src/pages/study/index.ts
@@ -1,20 +1,19 @@
-
-import Page from 'page'
-import testMixin from '@/mixins/test'
-import tabView from '@/components/tabView/tabview'
+import Page from "page";
+import testMixin from "@/mixins/test";
+import tabView from "@/components/tabView/tabview";
 
 export default class StudyPage extends Page {
-    config = {
-        navigationBarTitleText: '学习'
-    }
-    components = {
-        'tab-view': tabView
-    }
+  config = {
+    navigationBarTitleText: "学习"
+  };
+  components = {
+    "tab-view": tabView
+  };
 
-    data = {}
-    computed = {}
-    methods = {}
-    mixins = [testMixin]
-    events = {}
-    onLoad() { }
+  data = {};
+  computed = {};
+  methods = {};
+  mixins = [testMixin];
+  events = {};
+  onLoad() {}
 }
