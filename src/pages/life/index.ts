583d3027393695a9eb5077a9899d83a2a5622097
ElderJames <shunjiey@hotmail.com>
2018-02-05 10:26:10 +0800

    feat(swiper): 增加swiper组件

--- src/pages/life/index.ts
+++ src/pages/life/index.ts
@@ -1,16 +1,35 @@
-import Page from 'page'
-import testMixin from '@/mixins/test'
+import Page from "page";
+import testMixin from "@/mixins/test";
+import Swiper from "@/components/swiper/swiper";
 
 export default class LifePage extends Page {
-    config = {
-        navigationBarTitleText: '生活'
-    }
-    components = {}
+  config = { navigationBarTitleText: "生活" };
+  components = { swiper: Swiper };
 
-    data = {}
-    computed = {}
-    methods = {}
-    mixins = [testMixin]
-    events = {}
-    onLoad() { }
+  data = {
+    swiper: {
+      direction: "horizontal", //"vertical",
+      slideLength: 3,
+      onInit(weswiper) {},
+      onTouchStart(weswiper, event) {},
+      onTouchMove(weswiper, event) {},
+      onTouchEnd(weswiper, event) {},
+      onSlideChangeStart(weswiper) {},
+      onSlideChangeEnd(weswiper) {},
+      onTransitionStart(weswiper) {},
+      onTransitionEnd(weswiper) {},
+      onSlideMove(weswiper) {},
+      onSlideNextStart(weswiper) {},
+      onSlideNextEnd(weswiper) {},
+      onSlidePrevStart(swiper) {},
+      onSlidePrevEnd(weswiper) {}
+    }
+  };
+  computed = {};
+  methods = {};
+  mixins = [testMixin];
+  events = {};
+  onLoad() {
+    setTimeout(() => this.$invoke("weSwiper", "slideTo", 2), 3000);
+  }
 }
